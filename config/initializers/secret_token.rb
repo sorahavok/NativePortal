e85743e2944bbe8034fd4e26253928fb41095cc6
Scott Finkel <sora.havok@gmail.com>
2013-03-16 07:30:36 -0700

    INIT

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NativePortal::Application.config.secret_token = '44e2adfb626a7d34b8f48fa623a7eede6a05a7349810075c4bf8623b425f98ec387826b5b2767d324d1577858bab3f1cb308f2afd2a1bd736bd847d58e056801'
