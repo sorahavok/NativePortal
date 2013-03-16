ab79b86267dbcd18815b08d8668e97e1d6bdfecd
Scott Finkel <sora.havok@gmail.com>
2013-03-16 08:04:35 -0700

    seeds and stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+News.create(:title => 'New Title', :content => "This is Content")
